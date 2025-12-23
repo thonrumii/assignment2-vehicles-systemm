b775c1fc104abe44b88bcc3ef41375a245aa7a05
aidad <aidadushayeva@gmail.com>
2025-12-23 18:02:48 +0500

    Initial commit:project structure

---
+++ src/ElectricTruck.java
@@ -0,0 +1,16 @@
+public class ElectricTruck extends Truck{
+    private double batteryCap;
+    public ElectricTruck(String brand, int year, double capacity, int numAxles, double batteryCap){
+        super(brand, year, capacity, numAxles);
+        this.batteryCap=batteryCap;
+    }
+    @Override
+    public void startEngine() {
+        System.out.println("Electric truck powered on");
+    }
+
+    @Override
+    public void stopEngine() {
+        System.out.println("Electric truck powered off");
+    }
+}
